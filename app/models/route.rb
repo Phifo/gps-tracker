02fa3d108c9d144079bea33f0374188014da0576
Christopher Fernández <fernandez.chl@gmail.com>
2020-02-10 22:28:47 -0300

    fix: fix route filter

--- app/models/route.rb
+++ app/models/route.rb
@@ -5,6 +5,7 @@ class Route < ApplicationRecord
 
   belongs_to :vehicle
   belongs_to :route_definition
+  has_many :waypoints
 
   enum status: %i[finalized on_route incomplete]
 
@@ -12,7 +13,7 @@ class Route < ApplicationRecord
                         joins(:route_definition)
                           .where(route_definitions: { name: name })
                       }
-  filter :date, ->(date) { where(started_at: date) }
+  filter :date, ->(date) { where(started_at: date.to_date.beginning_of_day..date.to_date.end_of_day) }
   filter :vehicle_identifier, lambda { |identifier|
                                 joins(:vehicle)
                                   .where(vehicles: { identifier: identifier })
